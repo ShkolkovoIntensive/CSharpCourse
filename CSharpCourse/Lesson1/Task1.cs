27aaad140bf93519b3dc739f8d03e7215cb29366
Sergey Gordeev <duamaster1504@yandex.ru>
2020-11-18 19:32:26 +0300

    Task 1 is done. Added a lesson folder.

---
+++ CSharpCourse/Lesson1/Task1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSharpCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Hello, {Name}!");
+        }
+    }
+}
