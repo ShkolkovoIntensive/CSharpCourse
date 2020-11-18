e0dfced71e6e093488f35b2e3f9359f53cf77a4e
Sergey Gordeev <duamaster1504@yandex.ru>
2020-11-18 20:23:10 +0300

    Task 3 is done.

---
+++ CSharpCourse/Lesson1/Task3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSharpCourse.Lesson1
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the symbol: ");
+            char a = (char)Console.Read();
+            Console.WriteLine($"The next symbol is '{(char)++a}'");
+        }
+    }
+}
