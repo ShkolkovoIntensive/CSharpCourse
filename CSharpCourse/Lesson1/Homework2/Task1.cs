41959976dde81c911d7e143a372769a15dbf10e4
Sergey Gordeev <duamaster1504@yandex.ru>
2020-11-19 09:32:32 +0300

    Task1 from Homework2 is done.

---
+++ CSharpCourse/Lesson1/Homework2/Task1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourse.Lesson1.Homework2
+{
+    class Task1
+    {
+        enum BankAccounts
+        {
+            Current,
+            Credit,
+            Deposit,
+            Budgetary
+        }
+        static void Main(string[] args)
+        {
+            BankAccounts Account = BankAccounts.Credit;
+            Console.WriteLine($"Your bank account type is {Account}");
+        }
+    }
+}
